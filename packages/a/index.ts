dbaa5cf918e0a6d2a8e754ec300cc3a05fa92c32
L.Rain <bxy2997@sina.com>
2024-01-24 15:37:40 +0800

    feat: init

---
+++ packages/a/index.ts
@@ -0,0 +1,8 @@
+export function useTestN() {
+  const cc = 1;
+  const dd = 2;
+  return {
+    cc: cc,
+    dd: dd,
+  };
+}
---
+++ packages/b/index.ts
@@ -0,0 +1,7 @@
+import { useTestN } from "@mono/a";
+
+const test = useTestN();
+const test2 = {
+  dd: test.cc,
+  vv: test.dd,
+};
